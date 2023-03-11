579b99fac875f7631e313a54abb87f79ce65b520
Chad Bumstead <cbumstead@sureco.com>
2023-03-11 12:31:08 -0800

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -8,10 +8,6 @@ import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ChatGPT Batch Refactor" is now active!');
-
     const getOpenAIKey = async (forceChange: boolean = false) => {
 		const secrets = context.secrets;
 		const oldOpenAIKey = await secrets.get("openAIKey");
