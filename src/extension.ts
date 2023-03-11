3764343d3d9262f7ab5e59cdb91700019dc55927
Chad Bumstead <cbumstead@sureco.com>
2023-03-11 11:38:47 -0800

    select files from folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-//import axios from 'axios';
 
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
-import { open } from 'fs';
 
+//TODO: Ask for API key
 const configuration = new Configuration({
   apiKey: "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" // process.env.OPENAI_API_KEY,
 });
@@ -19,69 +18,62 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ChatGPT Batch Refactor" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatgpt-batch-refactor.chat', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Welcome to ChatGPT Batch Refactor!');
+    const command = 'chatgptBatchRefactor.refactorSelected';
+    context.subscriptions.push(vscode.commands.registerCommand(command, async (ctx) => {
+        const options: vscode.OpenDialogOptions = {
+        canSelectMany: true,
+        openLabel: 'Refactor',
+        // set the defaultUri to the selected folder
+        defaultUri: ctx?.fsPath ? vscode.Uri.file(ctx.fsPath) : undefined,
+        filters: {
+            'All files': ['*']
+        },
+        };
+        const uris = await vscode.window.showOpenDialog(options);
+        if (uris) {
+            vscode.window.showInformationMessage('ChatGPT Batch Refactor found ' + uris.length + ' files to refactor');
 
-        const defaultPrompt = "Refactor the persons data folder to use Typescript and Prisma.";
+            const defaultPrompt = "Refactor to use Typescript and Prisma.";
 
-        const input = await vscode.window.showInputBox({ prompt: 'Enter your instructions for ChatGPT Batch Refactor', value: defaultPrompt }) ?? defaultPrompt;
+            const input = await vscode.window.showInputBox({ prompt: 'Enter your instructions for ChatGPT Batch Refactor', value: defaultPrompt }) ?? defaultPrompt;
 
-        const messages: ChatCompletionRequestMessage[] = [
-            {"role": "system", "content": "You are a ChatGPT vs code extension that can do multi-file refactoring. When you receive a request to refactor code always put any notes in code comments so the file will still build."},
-            {"role": "user", "content": input },
-        ];
+            const messages: ChatCompletionRequestMessage[] = [
+                {"role": "system", "content": "You are a ChatGPT vs code extension that can do multi-file refactoring. When you receive a request to refactor code always put any notes in code comments so the file will still build."},
+                {"role": "user", "content": input },
+            ];
 
-        try {
+            try {
+                // vscode.window.showInformationMessage('ChatGPT Batch Refactor found ' + uris.length + ' files to refactor');
 
-            //vscode.window.showInformationMessage('ChatGPT Batch Refactor is working on your request to ' + input);
+                // Perform refactoring on the selected file/folder.
 
-            // const completion = await openai.createChatCompletion({
-            //     model: 'gpt-3.5-turbo',
-            //     messages: messages
-            // });
-
-            // TODO: Let the developer choose the files to refactor in vscode
-
-
-
-            // TODO: Find the files to refactor
-            const files = await vscode.workspace.findFiles('**/data/persons/**', '**/node_modules/**');
-
-            vscode.window.showInformationMessage('ChatGPT Batch Refactor found ' + files.length + ' files to refactor');
-
-            // For each file, ask ChatGPT to the refactoring
-            for (const file of files) {
-                // Read the file
-                const fileContent = await vscode.workspace.fs.readFile(file);
-                // Ask ChatGPT to refactor the file
-                const response = await openai.createChatCompletion({
-                    model: 'gpt-3.5-turbo',
-                    messages:[ ...messages,
-                        {"role": "assistant", "content": fileContent.toString()},
+                // For each file, ask ChatGPT to the refactoring
+                for (const uri of uris) {
+                    // Read the file
+                    const fileContent = await vscode.workspace.fs.readFile(uri);
+                    // Ask ChatGPT to refactor the file
+                    const response = await openai.createChatCompletion({
+                        model: 'gpt-3.5-turbo',
+                        messages:[ ...messages,
+                            {"role": "assistant", "content": fileContent.toString()},
                     ]});
-                console.log(response.data.choices[0].message?.content);
-                if(response.data.choices[0].message?.content){
-                    const content = refactorGraphQL(response.data.choices[0].message.content);
-                    // Write the refactored file
-                    await vscode.workspace.fs.writeFile(file, Buffer.from(content));
-                }
-                break; // Only run on one file for now
-            }    
+                    console.log(response.data.choices[0].message?.content);
+                    if(response.data.choices[0].message?.content){
+                        const content = refactorGraphQL(response.data.choices[0].message.content);
+                        // Write the refactored file
+                        await vscode.workspace.fs.writeFile(uri, Buffer.from(content));
+                    }
+                    break; // Only run on one file for now
+                }    
 
-            vscode.window.showInformationMessage('ChatGPT Batch Refactor has finished your request to ' + input);
+                vscode.window.showInformationMessage('ChatGPT Batch Refactor has finished your request to ' + input);
 
-        } catch (error) {
-            console.error(error);
-        }
+            } catch (error) {
+                console.error(error);
+            }
 
-	});
-
-	context.subscriptions.push(disposable);
+            }
+    }));
 }
 
 // Function to handle the GraphQL refactoring request
