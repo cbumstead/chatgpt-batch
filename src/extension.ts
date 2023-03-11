81f631a3982fc2209a543a934fa65e3b60a52db5
Chad Bumstead <cbumstead@sureco.com>
2023-03-11 12:29:36 -0800

    get API key from user

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,6 @@ import * as vscode from 'vscode';
 
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
-//TODO: Ask for API key
-const configuration = new Configuration({
-  apiKey: "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" // process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -18,8 +12,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ChatGPT Batch Refactor" is now active!');
 
+    const getOpenAIKey = async (forceChange: boolean = false) => {
+		const secrets = context.secrets;
+		const oldOpenAIKey = await secrets.get("openAIKey");
+		if (oldOpenAIKey && !forceChange) {
+			return oldOpenAIKey;
+		}
+		const key = await vscode.window.showInputBox({
+			title: "OpenAI API Key",
+			prompt: "Enter your OpenAI API key",
+			ignoreFocusOut: true,
+		});
+		if (!key) {
+			return "";
+		}
+		secrets.store("openAIKey", key);
+		return key;
+	};
+
     const command = 'chatgptBatchRefactor.refactorSelected';
-    context.subscriptions.push(vscode.commands.registerCommand(command, async (ctx) => {
+    const refactorCommand = vscode.commands.registerCommand(command, async (ctx) => {
+        const apiKey = await getOpenAIKey();
+        const configuration = new Configuration({
+            apiKey: apiKey,
+          });
+        const openai = new OpenAIApi(configuration);
+
         const options: vscode.OpenDialogOptions = {
         canSelectMany: true,
         openLabel: 'Refactor',
@@ -43,10 +61,6 @@ export function activate(context: vscode.ExtensionContext) {
             ];
 
             try {
-                // vscode.window.showInformationMessage('ChatGPT Batch Refactor found ' + uris.length + ' files to refactor');
-
-                // Perform refactoring on the selected file/folder.
-
                 // For each file, ask ChatGPT to the refactoring
                 for (const uri of uris) {
                     // Read the file
@@ -63,17 +77,30 @@ export function activate(context: vscode.ExtensionContext) {
                         // Write the refactored file
                         await vscode.workspace.fs.writeFile(uri, Buffer.from(content));
                     }
-                    break; // Only run on one file for now
+                    break; //TODO: Only run on one file for now
                 }    
 
                 vscode.window.showInformationMessage('ChatGPT Batch Refactor has finished your request to ' + input);
 
             } catch (error) {
+                vscode.window.showInformationMessage('ChatGPT Batch Refactor failed to refactor your code.  Check your API key and try again.');
                 console.error(error);
             }
 
             }
-    }));
+    });
+
+    const updateOpenAIKey = vscode.commands.registerCommand(
+		"chatgptBatchRefactor.changeOpenAIAPIKey",
+		async () => {
+			await getOpenAIKey(true);
+		},
+	);
+
+    context.subscriptions.push(
+		updateOpenAIKey,
+        refactorCommand
+	);
 }
 
 // Function to handle the GraphQL refactoring request
