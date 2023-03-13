5116e68a67f7c712de2136a45ca27372c3013744
Chad Bumstead <cbumstead@sureco.com>
2023-03-12 19:57:08 -0700

    add cases

--- src/extension.ts
+++ src/extension.ts
@@ -55,16 +55,16 @@ export function activate(context: vscode.ExtensionContext) {
                 ignoreFocusOut: true }) ?? defaultPrompt;
 
 
-            vscode.window.showInformationMessage('ChatGPT Refactor is now refactoring.  Please wait.');
+            vscode.window.showInformationMessage('ChatGPT is now refactoring.  Please wait.');
 
             const messages: ChatCompletionRequestMessage[] = [
-                {"role": "system", "content": "You are a ChatGPT vs code extension that can do multi-file refactoring. When you receive a request to refactor code always put any notes in code comments so the file will still build."},
+                {"role": "system", "content": "Pretend you are a professional software architect that can do code refactoring.  You will receive specific requests to refactor code. You will always explain why you chose to make the changes you did in the refactoring. Include these explanations inline with the code in comments each line beginning with //."},
                 {"role": "user", "content": input },
             ];
-
+ 
             try {
                 // For each file, ask ChatGPT to the refactoring
-                for (const uri of uris) {
+                for (const uri of uris) { 
                     // Read the file
                     const fileContent = await vscode.workspace.fs.readFile(uri);
                     // Ask ChatGPT to refactor the file
@@ -107,19 +107,113 @@ export function activate(context: vscode.ExtensionContext) {
 
 // Function to handle the GraphQL refactoring request
 function refactorGraphQL(content: string) : string {
-    // TODO: USE REGEX GROUP to find matching ``` and ending ```
-    // Find all the lines before ``` and after ``` and add comment markers
-    // const [before, code, after] = content.split('```');
-    // const refactoredCode = [
-    //     '//',
-    //     before.split('\n').map(line => '// ' + line).join('\n'),
-    //     ...code,
-    //     after.split('\n').map(line => '// ' + line).join('\n'),
-    // ].join('');
-    // replace all ``` with //
+    // There are two types of code blocks in the response:
 
-    const refactoredCode = `//${content.replace(/```/g, '//')}`;
-    return refactoredCode;
+    // The first format has a single comment line and a empty line before the code block
+    // And a single empty line and comment line after the code block
+    
+    // Refactored code using Typescript and Prisma:
+    //
+    // import { PrismaClient } from '@prisma/client';
+    // import stringScore from 'string_score';
+    // ...
+    //
+    // Note: This is just an initial refactoring, there might be other optimizations and improvements that could be done.
+
+    // This second format uses code blocks
+
+    // Refactored code using Typescript and Prisma:
+    // ```typescript
+    // import { PrismaClient } from '@prisma/client';
+    // import stringScore from 'string_score';
+    // ```
+    // Explanation:
+    //
+    // 1. Added a type `InsertRecord` and `UpdateRecord` that represents the structure of the `CensusRecord` object. 
+    // 2. Imported `Prisma` to interact with the database.
+    // 3. Modified the `createRecord`, `mergeRecord`, and `ignoreRecord` functions to interact with the Prisma client.
+    // 4. Added return types to each function
+    // 5. Created two new functions `getFieldScore` and `getMatches` to find the match and to score each column against another.
+    // 6. Made `isInsert` and `isUpdate` functions return a narrow Boolean to properly and safely type out the object in `upsertCensusData`
+    // 7. Updated the `upsertCensusData` function to interact with the Prisma client when modifying the data, and to return a Promise of `CensusRecord[]`.
+    //
+    // Modifications were made to accommodate the `Prisma` client and Typescript. Turning what once was an error-prone project into more manageable code.
+    
+    if(content.indexOf('```') > 0){
+        // Find all the lines before ``` and after ``` and add comment markers
+        // Split all lines into an array
+        const lines = content.split('\n');
+        let startFound = false;
+        let endFound = false;
+        // For each line
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+
+            if(line.startsWith('```')){
+                if(!startFound && !endFound){
+                    startFound = true;
+                } else {
+                    if(startFound && !endFound){
+                        endFound = true;
+                    }
+                }
+                lines[i] = '//';
+            }
+
+            // Add comment markers to the line if it is not between the ``` markers
+            if(!startFound && endFound){
+                lines[i] = '// ' + line;
+            }
+
+            if(startFound && endFound){
+                startFound = false;
+                endFound = false;
+            }
+        }
+
+        return lines.join('\n');
+    } else {
+        // Split all lines into an array
+        const lines = content.split('\n');
+        // For each line
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+
+            if(i === 0){
+                // Add comment markers to the first and second line
+                lines[i] = '// ' + line;
+            }
+
+            // // Add comment markers to last line if line before is empty
+            // if(i === lines.length - 1){
+            //     if(lines[i-1] === '\n'){
+            //         lines[i] = '// ' + line;
+            //     }
+            // }
+
+            // if(i === lines.length - 1){
+            //     lines[i] = '// ' + line;
+            // }
+        }
+
+        // For each line in reverse
+        for (let i = lines.length - 1; i >= 0; i=i-2) {
+            const line = lines[i];
+
+            // Add comment markers to line and line before if line before is empty
+            if(i === lines.length - 1){
+                if(lines[i-1] === ''){
+                    lines[i] = '// ' + line;
+                    lines[i-1] = '// ' + lines[i-1];
+                } else {
+                    // Stop when we reach the first code block
+                    break;
+                }
+            }
+        }
+
+        return lines.join('\n');
+    }
 }
 
 // This method is called when your extension is deactivated
